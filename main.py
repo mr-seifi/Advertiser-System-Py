b6f35cb62057c4533fbca60d566db68e38a0969d
Amin_Seifi <aminseiifi@gmail.com>
2021-12-25 11:37:53 +0330

    Update Main

---
+++ main.py
@@ -0,0 +1,13 @@
+from BaseAdvertising import BaseAdvertising
+
+if __name__ == '__main__':
+
+    baseAd = BaseAdvertising()
+    baseAd.describeMe()
+    baseAd.incClicks()
+    baseAd.incClicks()
+    baseAd.incViews()
+    baseAd.describeMe()
+
+    baseAd2 = BaseAdvertising()
+    baseAd2.describeMe()

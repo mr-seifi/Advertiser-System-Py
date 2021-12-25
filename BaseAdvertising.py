f2687322de6291efc177b7998dfee3cfbae0cdf7
Amin_Seifi <aminseiifi@gmail.com>
2021-12-25 21:04:13 +0330

    Delete last_id from BaseAdvertising

--- BaseAdvertising.py
+++ BaseAdvertising.py
@@ -1,17 +1,13 @@
 class BaseAdvertising:
     """Docstring"""
 
-    __last_id = 0
-
     def __init__(self, clicks=0, views=0):
         assert(clicks >= 0 and views >= 0), 'Clicks or Views should be positive!'
         self.__clicks = clicks
         self.__views = views
-        BaseAdvertising.__last_id += 1
-        self.__id = BaseAdvertising.__last_id
 
     def __str__(self):
-        return f'ID: {self.__id}\tCLICKS: {self.__clicks}\tVIEWS: {self.__views}'
+        return f'CLICKS: {self.__clicks}\tVIEWS: {self.__views}'
 
     @property
     def clicks(self):

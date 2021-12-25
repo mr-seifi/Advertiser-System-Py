45215a979efabf818b741e0a76b035c9544102ee
Amin_Seifi <aminseiifi@gmail.com>
2021-12-25 21:05:33 +0330

    Use One underscore instead of two of it

--- Ad.py
+++ Ad.py
@@ -4,7 +4,7 @@ from BaseAdvertising import BaseAdvertising
 class Ad(BaseAdvertising):
     """docstring"""
 
-    __last_id = 0
+    _last_id = 0
 
     def __init__(self, title, advertiser, imgUrl='', link='', clicks=0, views=0):
         super().__init__(clicks, views)
@@ -12,43 +12,43 @@ class Ad(BaseAdvertising):
         self.advertiser = advertiser
         self.imgUrl = imgUrl
         self.link = link
-        Ad.__last_id += 1
-        self.__id = Ad.__last_id
+        Ad._last_id += 1
+        self._id = Ad._last_id
 
     def __str__(self):
-        return f'ID: {self.__id}\tTITLE: {self.title}\tADVERTISER_NAME: {self.advertiser.name}\tIMG_URL: {self.imgUrl}\tLINK: {self.link}\tCLICKS: {self.clicks}\tVIEWS: {self.views}'
+        return f'ID: {self._id}\tTITLE: {self.title}\tADVERTISER_NAME: {self.advertiser.name}\tIMG_URL: {self.imgUrl}\tLINK: {self.link}\tCLICKS: {self.clicks}\tVIEWS: {self.views}'
 
     @property
     def title(self):
-        return self.__title
+        return self._title
 
     @title.setter
     def title(self, title):
-        self.__title = title
+        self._title = title
 
     @property
     def imgUrl(self):
-        return self.__imgUrl
+        return self._imgUrl
 
     @imgUrl.setter
     def imgUrl(self, imgUrl):
-        self.__imgUrl = imgUrl
+        self._imgUrl = imgUrl
 
     @property
     def link(self):
-        return self.__link
+        return self._link
 
     @link.setter
     def link(self, link):
-        self.__link = link
+        self._link = link
 
     @property
     def advertiser(self):
-        return self.__advertiser
+        return self._advertiser
 
     @advertiser.setter
     def advertiser(self, advertiser):
-        self.__advertiser = advertiser
+        self._advertiser = advertiser
 
     def incClicks(self):
         super().incClicks()

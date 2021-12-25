c2ef64dd6fa8f1278922b002d865703f80da18c4
Amin_Seifi <aminseiifi@gmail.com>
2021-12-25 21:06:33 +0330

    Use One underscore instead of two of it

--- BaseAdvertising.py
+++ BaseAdvertising.py
@@ -3,25 +3,25 @@ class BaseAdvertising:
 
     def __init__(self, clicks=0, views=0):
         assert(clicks >= 0 and views >= 0), 'Clicks or Views should be positive!'
-        self.__clicks = clicks
-        self.__views = views
+        self._clicks = clicks
+        self._views = views
 
     def __str__(self):
-        return f'CLICKS: {self.__clicks}\tVIEWS: {self.__views}'
+        return f'CLICKS: {self._clicks}\tVIEWS: {self._views}'
 
     @property
     def clicks(self):
-        return self.__clicks
+        return self._clicks
 
     def incClicks(self):
-        self.__clicks += 1
+        self._clicks += 1
 
     @property
     def views(self):
-        return self.__views
+        return self._views
 
     def incViews(self):
-        self.__views += 1
+        self._views += 1
 
     def describeMe(self):
         print(self)
